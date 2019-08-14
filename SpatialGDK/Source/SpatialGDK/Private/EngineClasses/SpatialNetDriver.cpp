1cc21cae71a845998884056db0a82cd75cd4e74b
samiwh <sami@improbable.io>
2019-08-14 12:15:38 +0100

    Driver - null checks

--- SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialNetDriver.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialNetDriver.cpp
@@ -1226,15 +1226,13 @@ void USpatialNetDriver::TickDispatch(float DeltaTime)
 	// Not calling Super:: on purpose.
 	UNetDriver::TickDispatch(DeltaTime);
 
-	if (!Connection->IsConnected())
+	if (Connection == nullptr || !Connection->IsConnected())
 	{
 		return;
 	}
 
-	const auto& Worker = Connection->GetWorker();
-	Dispatcher->ProcessOps(Worker);
 	Connection->Advance();
-	return;
+	Dispatcher->ProcessOps(Connection->GetWorker());
 }
 
 void USpatialNetDriver::ProcessRemoteFunction(
@@ -1372,6 +1370,11 @@ void USpatialNetDriver::TickFlush(float DeltaTime)
 	// Tick the timer manager
 	{
 		TimerManager.Tick(DeltaTime);
+	}
+
+	if (Connection != nullptr && Connection->IsConnected())
+	{
+		Connection->FlushMessageToSend();
 	}
 
 	Super::TickFlush(DeltaTime);
